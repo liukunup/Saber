ea76217bd707b5aed7cb6fb8ee9ad5a8595e4340
Liu Kun <liukunup@163.com>
2022-07-24 18:05:58 +0800

    完成run.sh的编写

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# shellcheck disable=SC2039
 source venv/bin/activate
 
 while true; do
     flask deploy
+    # shellcheck disable=SC2181
     if [[ "$?" == "0" ]]; then
         break
     fi
@@ -11,4 +13,4 @@ while true; do
     sleep 5
 done
 
-exec gunicorn -b :5000 --access-logfile - --error-logfile - main:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - application:app
